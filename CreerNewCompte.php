c612949667288ff5c3c7f22f9b36f0b2643c81cd
Tongh <Wenxiao0015@icloud.com>
2017-12-12 11:37:20 +0100

    une page pour créer le compte
    pas encore fini la partie des enregistrer les données dans SQL, je vais le faire, et après je vais ajouter quelques information obligatoire pour créer les comtpe

---
+++ CreerNewCompte.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Page de créer nouveau compte</title>
+	<link rel="stylesheet" type="text/css" href="css/headerBodyFooterFixed.css">
+</head>
+<body>
+	<?php 
+		function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+
+		$nomErr = $prenomErr = $genderErr = $emailErr = "";
+		$nom = $prenom = $email = $gender = "";
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (empty($_POST["nom"])) {
+				$nomErr = "Nom est requis";
+			} else {
+				$nom = test_input($_POST["nom"]);
+				if (!preg_match("/^[-a-zA-Z ]*$/", $nom)) {
+					$nomErr = "Autorisé seulement les lettres et l'espace!";
+				}
+			}
+
+			if (empty($_POST["prenom"])) {
+				$prenomErr = "Préom est requis";
+			} else {
+				$prenom = test_input($_POST["prenom"]);	
+				if (!preg_match("/^[-a-zA-Z ]*$/", $prenom)) {
+					$prenomErr = "Autorisé seulement les lettres et l'espace!";
+				}
+			}		
+
+			if (empty($_POST["email"])) {
+				$emailErr = "E-mail est requis";
+			} else {
+				$email = test_input($_POST["email"]);
+				if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)) {
+					$emailErr = "Le format de courriel est invalide";
+				}
+			}	
+
+			if (empty($_POST["gender"])) {
+				$genderErr = "Gender est requis";
+			} else {
+				$gender = test_input($_POST["gender"]);	
+			}
+		}
+
+ 	?>
+	<div class="partieInfo">
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			Nom   : <input type="text" name="nom" value="<?php echo $nom;?>"> 
+			<span class="error"> *<?php echo $nomErr;?></span><br><br>
+			Prénom: <input type="text" name="prenom" value="<?php echo $prenom;?>">
+			<span class="error"> *<?php echo $prenomErr;?></span><br><br>
+			E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+			<span class="error"> *<?php echo $emailErr;?></span><br><br>
+			Gender: <input type="radio" name="gender" <?php if (isset($gender) && $gender=="femme") echo "checked";?> value="femme"> Femme 
+					<input type="radio" name="gender" <?php if (isset($gender) && $gender=="homme") echo "checked";?> value="homme"> Homme
+					<span class="error"> *<?php echo $genderErr;?></span><br><br>
+			<input type="submit" name="submit" value="Envoyer">
+		</form>
+	</div>
+</body>
+</html>
