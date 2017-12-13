efba4bd2bfbbd805104c6b1034e532c1538795ed
Tongh <wenxiao0015@icloud.com>
2017-12-13 18:51:59 +0100

    petit à petit nouceau compte

--- CreerNewCompte.php
+++ CreerNewCompte.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 	<?php 
+		require("../db_config.php");
+		
 		function test_input($data) {
 			$data = trim($data);
 			$data = stripslashes($data);
@@ -14,8 +16,8 @@
 			return $data;
 		}
 
-		$nomErr = $prenomErr = $genderErr = $emailErr = $loginErr = $mdpErr = $mdpCErr = "";
-		$nom = $prenom = $email = $gender = $login = $mdp = $mdpC = "";
+		$nomErr = $prenomErr = $typeUErr = $emailErr = $loginErr = $mdpErr = $mdpCErr = $teleErr = "";
+		$nom = $prenom = $email = $typeU = $login = $mdp = $mdpC = $tele = "";
 		$allValider = 0;
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -46,7 +48,7 @@
 			} else {
 				$login = test_input($_POST["login"]);	
 				if (!preg_match("/^[-_a-zA-Z0-9 ]*$/", $login)) {
-					$prenomErr = "Autorisé seulement les lettres, l'espace, les chiffres, '-' et '_'!";
+					$loginErr = "Autorisé seulement les lettres, l'espace, les chiffres, '-' et '_'!";
 				} else {
 					$allValider++;
 				}
@@ -55,21 +57,26 @@
 			if (empty($_POST["mdp"])) {
 				$mdpErr = "Mot de passe est requis";
 			} else {
-				$login = test_input($_POST["login"]);	
-				if (!preg_match("/^[-_a-zA-Z0-9 ]*$/", $login)) {
-					$prenomErr = "Autorisé: a-zA-Z0-9-_";
+				$mdp = test_input($_POST["mdp"]);	
+				if (!preg_match("/^[-_a-zA-Z0-9 ]*$/", $mdp)) {
+					$mdpErr = "Autorisé: a-zA-Z0-9-_";
 				} else {
 					$allValider++;
 				}
 			}
 
 			if (empty($_POST["mdpC"])) {
-				$loginErr = "Vous devez confirmer votre mot de passe!";
-				$login = test_input($_POST["login"]);	
-				if (!preg_match("/^[-_a-zA-Z0-9 ]*$/", $login)) {
-					$prenomErr = "Autorisé seulement les lettres, l'espace, les chiffres, '-' et '_'!";
+				$mdpCErr = "Vous devez confirmer votre mot de passe!";
+			} else {
+				$mdpC = test_input($_POST["mdpC"]);	
+				if (!preg_match("/^[-_a-zA-Z0-9 ]*$/", $mdpC)) {
+					$mdpCErr = "Autorisé: a-zA-Z0-9-_ ";
 				} else {
-					$allValider++;
+					if ($mdp == $mdpC) {
+						$allValider++;
+					} else {
+						$mdpCErr = "Les deux mots de passe ne sont pas le même!";
+					}
 				}
 			}	
 
@@ -83,21 +90,47 @@
 				 else {
 					$allValider++;
 				}
+			}
+
+			if (empty($_POST["tele"])) {
+				$teleErr = "E-mail est requis";
+			} else {
+				$tele = test_input($_POST["tele"]);
+				if (!preg_match("/([0-9]+)/", $tele)) {
+					$teleErr = "Le format de tele est invalide";
+				}
+				 else {
+					$allValider++;
+				}
 			}	
 
-			if (empty($_POST["gender"])) {
-				$genderErr = "Gender est requis";
+			if (empty($_POST["typeU"])) {
+				$typeUErr = "Type de Utilisateur est requis";
 			} else {
-				$gender = test_input($_POST["gender"]);	
+				$typeU = test_input($_POST["typeU"]);	
 				$allValider++;
-				}
+			}
 
-			if ($allValider == 5) {
-				header("Location:finiCreerNewCompte.php");
+
+			if ($allValider == 8) {
+				echo $allValider . "<br>";
+				echo $nom . "<br>";
+				echo $prenom . "<br>";
+				echo $login . "<br>";
+				echo $mdp . "<br>";
+				echo $mdpC . "<br>";
+				echo $email . "<br>";
+				echo $tele . "<br>";
+				echo $typeU . "<br>";
+				$mdpMD5 = md5($mdp);
+				echo $mdpMD5 . "<br>";
+				$mdpCMD5 = md5($mdpC);
+				echo $mdpCMD5 . "<br>";
+				//header("Location:finiCreerNewCompte.php");
 			}
 		}
-
  	?>
+
 	<div class="partieInfo">
 		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 			Nom   : <input type="text" name="nom" value="<?php echo $nom;?>"> 
@@ -109,18 +142,21 @@
 			Login : <input type="text" name="login" value="<?php echo $login;?>">
 			<span class="error"> *<?php echo $loginErr;?></span>
 			<br><br>
-			Mot de passe : <input type="text" name="mdp" value="<?php echo $mdp;?>">
+			Mot de passe : <input type="password" name="mdp" value="<?php echo $mdp;?>">
 			<span class="error"> *<?php echo $mdpErr;?></span>
 			<br><br>
-			Comfirmation mot de passe : <input type="text" name="mdpC" value="<?php echo $mdpC;?>">
+			Comfirmation mot de passe : <input type="password" name="mdpC" value="<?php echo $mdpC;?>">
 			<span class="error"> *<?php echo $mdpCErr;?></span>
 			<br><br>
 			E-mail: <input type="text" name="email" value="<?php echo $email;?>">
 			<span class="error"> *<?php echo $emailErr;?></span>
 			<br><br>
-			Gender: <input type="radio" name="gender" <?php if (isset($gender) && $gender=="femme") echo "checked";?> value="femme"> Femme 
-					<input type="radio" name="gender" <?php if (isset($gender) && $gender=="homme") echo "checked";?> value="homme"> Homme
-					<span class="error"> *<?php echo $genderErr;?></span>
+			Téléphone: <input type="text" name="tele" value="<?php echo $tele;?>">
+			<span class="error"> *<?php echo $teleErr;?></span>
+			<br><br>
+			Type: <input type="radio" name="typeU" <?php if (isset($typeU) && $typeU=="client") echo "checked";?> value="client"> Client
+					<input type="radio" name="typeU" <?php if (isset($typeU) && $typeU=="admin") echo "checked";?> value="admin"> Administrateur
+					<span class="error"> *<?php echo $typeUErr;?></span>
 			<br><br>
 			<input type="submit" name="submit" value="Envoyer">
 		</form>
