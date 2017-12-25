e22ea6cdbacb15793b5e206ab5c9a704c97662be
Tongh <wenxiao0015@icloud.com>
2017-12-25 06:05:53 +0100

    le code pour creer compte SQL database

--- CreerNewCompte.php
+++ CreerNewCompte.php
@@ -10,13 +10,21 @@
 	<script type="text/javascript" src="jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 	<style type="text/css">
-		.error {
-  			color: #FF0000;
-		}
-		.NP {
-			color: #00FF66;
-		}
-	</style>
+	.error {
+		color: #FF0000;
+	}
+	.NP {
+		color: #00FF66;
+	}
+	section {
+		width: 50%; 
+		border: 1px solid black; 
+		margin-top: 10px;
+		margin-left: auto; 
+		margin-right: auto; 
+		padding-left: 10px;
+	}
+</style>
 </head>
 <body>
 	<?php include("header_accueil.html"); ?>
@@ -77,58 +85,59 @@
 			}
 			
 		}
- 	?>-->
+		?>-->
 
 
-	
 
-	<div class="DIYBody" style="text-align: center;">
-		
+
+		<div class="DIYBody">
+
 			
+			<section>
+				<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" onsubmit="return validerForm()">
+					<fieldset>
+						<legend> <strong>  Pour vous inscrire, renseignez les coordonnées suivantes:  </strong> </legend>
+						Nom   : <input type="text" id="nom" placeholder="Potter" name="nom" onchange="checkNom()">
+						<span id="NomErr" class="error"></span><span id="NomNP" class="NP"></span>
+						<br><br>
+						Prénom: <input type="text" id="prenom" name="prenom" placeholder="Harry" onchange="checkPrenom()">
+						<span id="PrenomErr" class="error"></span><span id="PrenomNP" class="NP"></span>
+						<br><br>
+						Pseudo : <input type="text" id="pseudo" placeholder="magicJack" name="login" onchange="checkPseudo()">
+						<span id="PseudoErr" class="error"></span><span id="PseudoNP" class="NP"></span>
+						<br><br>
+						Mot de passe : <input type="password" id="mdp" name="mdp" onchange="checkmdp()">
+						<span id="mdpErr" class="error"></span><span id="mdpNP" class="NP"></span>
+						<br><br>
+						Confirmation mot de passe : <input type="password" id="mdpC" name="mdpC" onchange="checkmdpC()">
+						<span id="mdpCErr" class="error"></span><span id="mdpCNP" class="NP"></span>
+						<br><br>
+						E-mail: <input type="text" id="mail" placeholder="xxxxxxxxx@gmail.com" name="email" onchange="checkMail()">
+						<span id="mailErr" class="error"></span><span id="mailNP" class="NP"></span>
+						<br><br>
+						Téléphone: <input type="text" id="tele" placeholder="06 00 00 00 00" name="tele" onchange="checkTele()">
+						<span id="teleErr" class="error"></span><span id="teleNP" class="NP"></span>
+						<br><br>
+						Type: <input type="radio" id="typeU" name="typeU" value="client"> Client
+						<input type="radio" name="typeU" value="admin"> Administrateur
+						<input type="text" id="codeV" placeholder="XXXXXXXX" name="codeV">
+						<span><a tabindex="0" class="btn btn-xs btn-info" role="button" data-toggle="popover" data-trigger="focus" data-content="le code que vous avez obtenu quand vous aviez acheté notre produit."><i class="fa fa-question"></i></a></span>
+						<br><br>
+						<input type="submit" name="submit" value="Envoyer">
+					</fieldset>
+				</form>
+			</div>
 
-		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" onsubmit="return validerForm()">
-			<fieldset>
-				<legend> <strong>  Pour vous inscrire, renseignez les coordonnées suivantes:  </strong> </legend>
-			Nom   : <input type="text" id="nom" name="nom" onchange="checkNom()">
-			<span id="NomErr" class="error"></span><span id="NomNP" class="NP"></span>
-			<br><br>
-			Prénom: <input type="text" id="prenom" name="prenom" onchange="checkPrenom()">
-			<span id="PrenomErr" class="error"></span><span id="PrenomNP" class="NP"></span>
-			<br><br>
-			Pseudo : <input type="text" id="pseudo" name="login" onchange="checkPseudo()">
-			<span id="PseudoErr" class="error"></span><span id="PseudoNP" class="NP"></span>
-			<br><br>
-			Mot de passe : <input type="password" id="mdp" name="mdp" onchange="checkmdp()">
-			<span id="mdpErr" class="error"></span><span id="mdpNP" class="NP"></span>
-			<br><br>
-			Confirmation mot de passe : <input type="password" id="mdpC" name="mdpC" onchange="checkmdpC()">
-			<span id="mdpCErr" class="error"></span><span id="mdpCNP" class="NP"></span>
-			<br><br>
-			E-mail: <input type="text" id="mail" name="email" onchange="checkMail()">
-			<span id="mailErr" class="error"></span><span id="mailNP" class="NP"></span>
-			<br><br>
-			Téléphone: <input type="text" id="tele" name="tele" onchange="checkTele()">
-			<span id="teleErr" class="error"></span><span id="teleNP" class="NP"></span>
-			<br><br>
-			Type: <input type="radio" id="typeU" name="typeU" value="client"> Client
-					<input type="radio" name="typeU" value="admin"> Administrateur
-					<input type="text" id="codeV" name="codeV">
-					<span><a tabindex="0" class="btn btn-xs btn-info" role="button" data-toggle="popover" data-trigger="focus" data-content="le code vous avez obtenu quand vous aviez acheté notre produit."><i class="fa fa-question"></i></a></span>
-			<br><br>
-			<input type="submit" name="submit" value="Envoyer">
-			</fieldset>
-		</form>
-	</div>
+			<div id="boutonretour">
+				<form action="connexion.php">
+					<input type="submit" value="Retournez vers mon espace client">
+				</form>
+			</div>
+		</section>
 
-	<div id="boutonretour">
-		<form action="connexion.php">
-			<input type="submit" value="Retournez vers mon espace client">
-		</form>
-	</div>
-
-	<script type="text/javascript">
-		$(function () { $("[data-toggle='popover']").popover(); }); 
-	</script>
-	<?php include "footer.html"; ?>
-</body>
-</html>
+		<script type="text/javascript">
+			$(function () { $("[data-toggle='popover']").popover(); }); 
+		</script>
+		<?php include "footer.html"; ?>
+	</body>
+	</html>
