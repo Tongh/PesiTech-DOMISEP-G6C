55552978851fa7aef4c91cbb69a2b5a350ffbeda
Yassine Dridi <dridi.yassine.yd@gmail.com>
2018-01-11 12:19:52 +0100

    Ajout d'1 bcle for->affiche le nb de piece choisi par l'user +traitement

--- gestion_capteurs.php
+++ gestion_capteurs.php
@@ -21,35 +21,46 @@
       <!-- <th>
           + Ajouter une pièce
       </th> -->
-      <th>
       <?php
-          echo " Ma pièce 1";
-      ?>
-      <form action="" />
-         <p>
-             <label for="piece">Veuillez sélectionner </br>
-             le type de pièce et le capteur associé:<br /> </label><br />
-             <select name="pays" id="pays">
-                 <option value="cuisine">Cuisine</option>
-                 <option value="Salon">Salon</option>
-                 <option value="chambre">Chambre </option>
-                 <option value="salle">Salle de bain</option>
-                 <option value="autre">Autre</option>
-             </select>
-             <select name="capteur" id="capteur">
-                 <option value="presence">Capteur de Présence</option>
-                 <option value="fumee">Capteur de Fumée</option>
-                 <option value="ouverture">Capteur d'Ouverture </option>
-                 <option value="camera">Caméra</option>
-                 <option value="luminosite">Capteur de Luminosité</option>
-                 <option value="fumee">Capteur d'Humidité'</option>
-                 <option value="ouverture">Capteur de Température </option>
-                 <option value="autre">Autre</option>
-             </select>
-             <input type="submit" />
-         </p>
-              </form>
-              </th>
+      $nb_piece=$_POST["nbpiece"];
+      for ($i=1; $i <=$nb_piece ; $i++) {
+
+        ?>
+
+        <th> Ma pièce <?php echo $i ?> : </br>
+          <form action="ajout_capteurs_bdd.php" method="POST" />
+           <p>
+               <label for="piece">Veuillez sélectionner </br>
+               le type de pièce et le capteur associé:<br /> </label><br />
+               <select name="type_piece" id="type_piece">
+                   <option value="cuisine">Cuisine</option>
+                   <option value="Salon">Salon</option>
+                   <option value="chambre">Chambre </option>
+                   <option value="salle">Salle de bain</option>
+                   <option value="autre">Autre</option>
+               </select>
+               <select name="type_capteur" id="capteur">
+                   <option value="presence">Capteur de Présence</option>
+                   <option value="fumee">Capteur de Fumée</option>
+                   <option value="ouverture">Capteur d'Ouverture </option>
+                   <option value="camera">Caméra</option>
+                   <option value="luminosite">Capteur de Luminosité</option>
+                   <option value="fumee">Capteur d'Humidité</option>
+                   <option value="ouverture">Capteur de Température </option>
+                   <option value="autre">Autre</option>
+               </select>
+             </br>
+               <label> Superficie de la pièce : <input type="text" name="superficie"/>
+          </th>
+
+    <?php
+    }
+    echo "<th><input type=\"submit\" value=\"Envoyer\" />
+      </p>
+     </form>
+     </th>";
+
+    ?>
   </table>
 
 
