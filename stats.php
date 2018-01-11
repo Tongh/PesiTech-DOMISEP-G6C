19f1ab6033a38abd85b7bfdfb7bf4f069b794fcc
maxvassal <max.vassal@gmail.com>
2018-01-11 11:58:09 +0100

    Modif CSS de la partie client

--- stats.php
+++ stats.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8"/>
   <!--dossier css-->
-  <link href="css/interv2.css" rel="stylesheet">
+  <link href="css/client.css" rel="stylesheet">
   <link href="css/profil.css" rel="stylesheet">
   <script src="https://use.fontawesome.com/e3c7c95da8.js"></script>
 </head>
