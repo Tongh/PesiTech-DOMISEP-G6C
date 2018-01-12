91c703806dce95e9d4e4915c414d3c6be0a673ed
maxvassal <max.vassal@gmail.com>
2018-01-12 11:46:46 +0100

    Indentation

--- header_accueil.php
+++ header_accueil.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-		<link rel="stylesheet" type="text/css" href="css/headerBodyFooterFixed.css"/> <!-- renvoi au fichier css header_accueil -->
+	<link rel="stylesheet" type="text/css" href="css/headerBodyFooterFixed.css"/> <!-- renvoi au fichier css header_accueil -->
 	<meta charset="utf-8"/>
 </head>
 <body>
-<header class="DIYheader">
- 	<a href="accueil.php"><img src="Image/logo_ez-home-moitie.png" alt="Logo" id="logo"></a> <!-- logo qui renvoi à la page d'accueil -->
-	<a href="connexion.php" class='connecterboutton'><img src="Image/profil.png" alt="Se Connecter" id="imgcn"/></a> <!--bouton connexion renvoi à la page de connexion -->
+	<header class="DIYheader">
+		<a href="accueil.php"><img src="Image/logo_ez-home-moitie.png" alt="Logo" id="logo"></a> <!-- logo qui renvoi à la page d'accueil -->
+		<a href="connexion.php" class='connecterboutton'><img src="Image/profil.png" alt="Se Connecter" id="imgcn"/></a> <!--bouton connexion renvoi à la page de connexion -->
 
 		<nav id="barre"> <!--menu navigationel -->
 			<ul>
 				<li id="qsn">
 					<a href="qui_sommes_nous.php"> Qui sommes-nous ? </a>
-        </li>
+				</li>
 
 				<li id="services">
 					<a href="nos_services.php"> Nos services </a>
@@ -21,7 +21,7 @@
 
 				<li id="acheter">
 					<a href="acheter.php"> Acheter </a>
-        </li>
+				</li>
 
 				<li id="contact">
 					<a href="nous_contacter.php"> Nous contacter </a>
@@ -31,6 +31,6 @@
 			</ul>
 		</nav>
 
-</header>
+	</header>
 </body>
 </html>
--- stats.php
+++ stats.php
@@ -19,67 +19,67 @@ include 'header_client.php';
 <body>
   <div id="DIYbody">
 
-  <form action="" />
-     <p>
-         <label for="piece">Veuillez sélectionner </br>
-         la pièce a afficher :<br /> </label><br />
-         <select name="pays" id="pays">
-             <option value="cuisine">Cuisine</option>
-             <option value="salon">Salon</option>
-             <option value="chambre">Chambre </option>
-             <option value="salle">Salle de bain</option>
-             <option value="autre">Autre</option>
-         </select>
-         <input type="submit" />
-       </br>
-     </br>
-   </br>
- </br>
-         <table>
-           <tr id="fline">
-             <th><strong>Capteur 1</strong></th>
-             <th colspan="3"><strong>Cuisine</strong></th>
-           </tr>
-           <tr>
-             <td>Type</td>
-             <td>Donnée actuelle</td>
-             <td>Historique </td>
-           </tr>
-           <tr>
-             <td>Température </td>
-             <td>21°C</td>
-             <td><a href="stats.php"><img src="Image\graph_ex.png" ></a></td>
-           </tr>
+    <form action="" />
+    <p>
+      <label for="piece">Veuillez sélectionner </br>
+        la pièce a afficher :<br /> </label><br />
+        <select name="pays" id="pays">
+          <option value="cuisine">Cuisine</option>
+          <option value="salon">Salon</option>
+          <option value="chambre">Chambre </option>
+          <option value="salle">Salle de bain</option>
+          <option value="autre">Autre</option>
+        </select>
+        <input type="submit" />
+      </br>
+    </br>
+  </br>
+</br>
+<table>
+  <tr id="fline">
+    <th><strong>Capteur 1</strong></th>
+    <th colspan="3"><strong>Cuisine</strong></th>
+  </tr>
+  <tr>
+    <td>Type</td>
+    <td>Donnée actuelle</td>
+    <td>Historique </td>
+  </tr>
+  <tr>
+    <td>Température </td>
+    <td>21°C</td>
+    <td><a href="stats.php"><img src="Image\graph_ex.png" ></a></td>
+  </tr>
 
-         </tr>
-       </table>
+</tr>
+</table>
 
-       <!-- <table>
-         <tr id="fline">
-           <th><strong>Capteur 2</strong></th>
-           <th colspan="3"><strong>Cuisine</strong></th>
-         </tr>
-         <tr>
-           <td>Type</td>
-           <td>Donnée actuelle</td>
-           <td>Historique </td>
-         </tr>
-         <tr>
-           <td>Luminosité </td>
-           <td>30%</td>
-           <td><a href="stats.php"><img src="Image\graph_ex.png" ></a></td>
-         </tr>
-       </tr>
-     </table> -->
+<!-- <table>
+<tr id="fline">
+<th><strong>Capteur 2</strong></th>
+<th colspan="3"><strong>Cuisine</strong></th>
+</tr>
+<tr>
+<td>Type</td>
+<td>Donnée actuelle</td>
+<td>Historique </td>
+</tr>
+<tr>
+<td>Luminosité </td>
+<td>30%</td>
+<td><a href="stats.php"><img src="Image\graph_ex.png" ></a></td>
+</tr>
+</tr>
+</table> -->
 
-         <?php
-         include 'footer_client.php';
-         ?>
+<?php
+include 'footer_client.php';
+?>
 </body>
 
 
-  <?php
-  include 'footer_client.php';
-  ?>
+<?php
+include 'footer_client.php';
+?>
 
 </html>
