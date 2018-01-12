10b7c797aa7a7ba91c0cb7a6e8c51849b8c62aeb
clem52 <33628258+clem52@users.noreply.github.com>
2018-01-12 10:27:17 +0100

    Update header_client.php

--- header_client.php
+++ header_client.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/headerBodyFooterFixedcss"> <!-- renvoi au fichier css header_accueil -->
+	<link rel="stylesheet" type="text/css" href="css/headerBodyFooterFixed.css"> <!-- renvoi au fichier css header_accueil -->
 
 </head>
 
