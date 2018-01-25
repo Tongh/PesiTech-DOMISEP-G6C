76ccc8f4a4962999437b11a1e9e301c78f9cd142
Tongh <wenxiao0015@icloud.com>
2018-01-26 00:20:22 +0100

    modification

---
+++ models/ess.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>sdfsdfqsdfsdfqsdfqsdfqsdfqsfqsdfqsdfqsdfsqdf</title>
+</head>
+<body>
+	<h1>save log</h1>
+	<?php 
+	require "Error.class.php";
+		function customError($errno, $errstr) {
+			$message = "<b>Error:</b> [$errno] $errstr";
+			$error = new G6C_Error($message);
+			$error -> saveLog();
+		}
+
+		set_error_handler("customError");
+
+		echo $test;
+		echo "hello";
+	 ?>
+</body>
+</html>
