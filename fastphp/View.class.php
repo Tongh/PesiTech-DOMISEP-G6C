ad40292c2ec81a4fbe07ce8719e404bafc3b674b
Tongh <wenxiao0015@icloud.com>
2018-01-26 07:44:28 +0100

    fini View.class.php

--- fastphp/View.class.php
+++ fastphp/View.class.php
@@ -15,6 +15,24 @@ class View {
 	}
 
 	function render() {
-		
+		extract($this -> _variables);
+		$defaultHeader = APP_PATH . 'views/header.php';
+		$defaultFooter = APP_PATH . 'views/footer.php';
+		$controllerHeader = APP_PATH . 'views/' . $this -> _controller . '/header.php';
+		$controllerFooter = APP_PATH . 'views/' . $this -> _controller . '/footer.php';
+
+		if (file_exists($controllerHeader)) {
+			include ($controllerHeader);
+		} else {
+			include ($defaultHeader);
+		}
+
+		include (APP_PATH . 'views/' . $this -> _controller . '/' . $this -> _action . '.php');
+
+		if (file_exists($controllerFooter)) {
+			include ($controllerFooter);
+		} else {
+			include ($defaultFooter);
+		}
 	}
 }
