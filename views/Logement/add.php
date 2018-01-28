354d3bef16e036a41dad03632914fef9a5abe00a
Tongh <wenxiao0015@icloud.com>
2018-01-28 13:33:03 +0100

    préparation logement insert

--- views/Logement/add.php
+++ views/Logement/add.php
@@ -1,8 +1,12 @@
 <div id='DIYbody'>
   <h1><?php echo $title ?><h1>
 
-  <form method="post" action="<?php echo "index.php?controller=Logement&action=attend"  ?>" onsubmit="return validerLogement()">
-  	<input type="text" placeholder="XXXXXXXX" name="value">
-  	<input type="submit" name="Ajouter">
-  </form>
+  <div class="mainFont">
+    <form method="post" action="<?php echo "index.php?controller=Logement&action=attend"  ?>" onsubmit="return validerLogement()">
+    	<input type="text" placeholder="XXXXXXXX" name="Address">
+    	<input type="text" placeholder="XXXXXXXX" name="dans_immeuble">
+    	<input type="text" placeholder="XXXXXXXX" name="value">
+    	<input type="submit" name="ajouter" value="Ajouter">
+    </form>
+  </div>
 </div>
