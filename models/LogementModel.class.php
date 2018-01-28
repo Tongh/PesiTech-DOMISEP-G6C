921459c77037f9f0bb6d5ded101ca787f264cb8a
Tongh <wenxiao0015@icloud.com>
2018-01-28 12:39:31 +0100

    logement model corriger bug

--- models/LogementModel.class.php
+++ models/LogementModel.class.php
@@ -9,10 +9,8 @@ class LogementModel extends Model {
 			$id_login = $result[0]['utilisateur']['id_utilisateur'];
 			$sql = 'SELECT * FROM `logement` WHERE 	`utilisateur_id utilisateur` = \'' . $id_login . '\'';
 			if ($result = $this -> query($sql)) {
-				return "Vous n'avez pas encore inscrire votre Logement tant que vos pièces et vos capteurs, vous pouvez maintenant en inscrire une !";
-			} else {
 				return $result;
-			}
+			} return "Vous n'avez pas encore inscrire votre Logement tant que vos pièces et vos capteurs, vous pouvez maintenant en inscrire une !";
 		} return "Le site rencontre des problème imprévue!";
 	}
 
