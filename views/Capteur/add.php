36b24beaac72442c37cc2ce39af63664deeffdb5
Tongh <wenxiao0015@icloud.com>
2018-01-30 19:31:06 +0100

    chenger form

--- views/Capteur/add.php
+++ views/Capteur/add.php
@@ -6,21 +6,15 @@
       <div class="mainFont">
         <form class="form-horizontal"  method="post" action="<?php echo "index.php?controller=Capteur&action=addForm"  ?>" onsubmit="return validerCapteur()" id="addCapteurForm">
           <div class="form-group form-group-sm">
-            <div class="formText"><label class="control-label col-sm-3" for="inputSuccess3">Superficie</label></div>
+            <div class="formText"><label class="control-label col-sm-3" for="inputSuccess3">Nom de Capteur</label></div>
             <div class="col-sm-9">
-              <input type="text" class="form-control" placeholder="17" name="superficie">
-            </div>
-          </div>
-          <div class="form-group form-group-sm">
-            <label class="control-label col-sm-3" for="inputGroupSuccess2">Nom</label>
-            <div class="col-sm-9">
-                <input type="text" class="form-control" placeholder="Ma Chambre" name="nom">
+              <input type="text" class="form-control" name="nom">
             </div>
           </div>
           <div class="form-group form-group-sm  ">
             <label class="control-label col-sm-3" for="inputGroupSuccess2">Type</label>
             <div class="col-sm-9">
-              <select name="type" class="form-control" form="addPieceForm">
+              <select name="type" class="form-control" form="addPieceForm" name="type_capteur">
                 <option value="Capteur de Présence">Capteur de Présence</option>
                 <option value="Capteur de Fumée">Capteur de Fumée</option>
                 <option value="Capteur d'Ouverture">Capteur d'Ouverture</option>
