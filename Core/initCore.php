62255750749d0a60c165032c3858848400ef858f
Tongh <wenxiao0015@icloud.com>
2018-01-31 17:04:48 +0100

    set APP_URL

--- Core/initCore.php
+++ Core/initCore.php
@@ -4,6 +4,7 @@ defined('ROOT') or define('ROOT', __DIR__.'/');
 defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . "/");
 defined('APP_DEBUG') or define('APP_DEBUG', false);
 defined('CONFIG_PATH') or define('CONFIG_PATH', APP_PATH . 'config/');
+define('APP_URL', 'http://localhost:8080/PesiTech-DOMISEP-G6C/');
 //defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'runtime/');
 
 const EXT = '.class.php';
