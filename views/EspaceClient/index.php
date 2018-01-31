39c760465e8ebfb363e6bba7593ddcc43cfd97f0
Tongh <wenxiao0015@icloud.com>
2018-01-31 19:37:10 +0100

    progresse

--- views/EspaceClient/index.php
+++ views/EspaceClient/index.php
@@ -4,7 +4,18 @@
 	<div class="btn-group">
 		<button type="button" class="btn btn-primary buttonVite" value="index.php?controller=EspaceClient" disabled="disabled"> Mon Profil </button>
 		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalMdp"> Mot de passe </button>
-		<button type="button" class="btn btn-primary buttonVite" value="index.php?controller=EspaceClient&action=installations"> Mos installations </button>
+		<div class="btn-group">
+			<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Mes installations
+				<span class="caret"></span>
+			</button>
+			<ul class="dropdown-menu">
+				<li><a href="#">Mes logements</a></li>
+				<li><a href="#">Mes Pièces</a></li>
+				<li><a href="#">Mes Capteurs</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">Mes achats</a></li>
+			</ul>
+		</div>
 	</div>
 
 	<div class="">
