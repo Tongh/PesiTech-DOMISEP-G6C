ccded9b49df220706fc1c2ef933b262b509c697f
Ranim <ranimkanaan@gmail.com>
2018-01-31 17:47:05 +0100

    .

--- config/db_config.php
+++ config/db_config.php
@@ -1,11 +1,11 @@
 ﻿<?php
 	/*
-	* Ce fichier permet la connexion à la base de donnée.
+	* Ce fichier permet la connexion à la base de données.
 	Vous devez le modifier en fonction de votre serveur, identifiant et mot de passe de votre serveur
-	et du nom donné à la base de donnée que vous aurez téléchargé au préalable.
+	et du nom donné à la base de données que vous aurez téléchargé au préalable.
 	*/
 	$mysql_server_name = "localhost"; 	// Modifier et mettre le nom votre nom de serveur
 	$mysql_username = "root"; 			// Modifier et mettre le nom d'utilisateur de votre serveur
 	$mysql_password = "";			// Modifier et mettre le mot de passe de votre serveur
-	$mysql_database = "Mydb";			// Modifier et mettre le nom que vous avez donné à la base de donnée
+	$mysql_database = "Mydb";			// Modifier et mettre le nom que vous avez donné à la base de données
  ?>
